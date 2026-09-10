dfa27888b9e4e2da077c9e1f3e7bb35b87a88ff7
bubbashub1 <bubbashub1@gmail.com>
2026-09-10 20:01:11 +0100

    Load shortcode asset compatibility fix

--- bubba-hub-production.php
+++ bubba-hub-production.php
@@ -20,6 +20,7 @@ require_once BUBBAHUB_DIR.'includes/class-finance-providers.php';
 require_once BUBBAHUB_DIR.'includes/class-finance-security.php';
 require_once BUBBAHUB_DIR.'includes/class-account-types.php';
 require_once BUBBAHUB_DIR.'includes/class-google-sync.php';
+require_once BUBBAHUB_DIR.'includes/class-shortcode-assets-fix.php';
 
 register_activation_hook(__FILE__, ['BubbaHub','activate']);
 register_deactivation_hook(__FILE__, ['BubbaHub','deactivate']);
