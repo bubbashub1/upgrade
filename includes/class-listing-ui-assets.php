f4fb3026a1277683f62bc3ae848a250375df1a86
bubbashub1 <bubbashub1@gmail.com>
2026-09-11 18:43:05 +0100

    Add responsive directory controls and view assets

--- includes/class-listing-ui-assets.php
+++ includes/class-listing-ui-assets.php
@@ -7,7 +7,9 @@ class BubbaHubListingUIAssets {
     public static function enqueue() {
         if (is_admin()) return;
         wp_enqueue_style('bubbahub-listings-card-upgrade', BUBBAHUB_URL . 'assests/css/listings-card-upgrade.css', [], BUBBAHUB_VERSION . '-card');
+        wp_enqueue_style('bubbahub-listings-directory-controls', BUBBAHUB_URL . 'assests/css/listings-directory-controls.css', ['bubbahub-listings-card-upgrade'], BUBBAHUB_VERSION . '-directory-controls');
         wp_enqueue_script('bubbahub-listings-upgrade', BUBBAHUB_URL . 'assests/js/listings-upgrade.js', [], BUBBAHUB_VERSION . '-listing', true);
+        wp_enqueue_script('bubbahub-listings-directory-controls', BUBBAHUB_URL . 'assests/js/listings-directory-controls.js', ['bubbahub-listings-upgrade'], BUBBAHUB_VERSION . '-directory-controls', true);
     }
 }
 BubbaHubListingUIAssets::boot();
