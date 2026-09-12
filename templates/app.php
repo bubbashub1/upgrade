baf2654642f949626d0d139528e547e1754f234d
bubbashub1 <bubbashub1@gmail.com>
2026-09-12 16:22:38 +0100

    Stage 4: add frontend navigation shell to app template

--- templates/app.php
+++ templates/app.php
@@ -1,1 +1,4 @@
+<?php
+if (shortcode_exists('bubbahub_navigation')) echo do_shortcode('[bubbahub_navigation]');
+?>
 <div id="bubbahub-app" class="bh-app"><noscript>Please enable JavaScript to use BubbaHub.</noscript></div>
