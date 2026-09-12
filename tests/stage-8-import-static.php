d856878560e87dff3b14a7b7f19d674478657b08
bubbashub1 <bubbashub1@gmail.com>
2026-09-12 17:06:38 +0100

    Stage 12: fix stale version assertion in CI static check

--- tests/stage-8-import-static.php
+++ tests/stage-8-import-static.php
@@ -4,7 +4,7 @@ $plugin = file_get_contents(__DIR__.'/../bubba-hub-production.php');
 $importer = file_get_contents(__DIR__.'/../includes/class-google-import.php');
 $checks = [
   strpos($plugin, "class-google-import.php") !== false,
-  strpos($plugin, "BUBBAHUB_VERSION,'4.1.6'") !== false,
+  preg_match("/BUBBAHUB_VERSION',\\s*'4\\.1\\.[0-9]+'/", $plugin) === 1,
   strpos($importer, 'class BubbaHubGoogleImport') !== false,
   strpos($importer, 'manage_bubbahub') !== false,
   strpos($importer, "post_type'=>'bh_group'") !== false,
